922027a1c871469ca2249177f253f7712fd39f38
rohitbk117 <rbiswakarma@albany.edu>
2024-01-29 20:25:36 -0500

    Team_blue
    encountering merge conflicts

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
-// Multiplication
-function multiply(a, b) {
-    return a * b;
-}
+// Division
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        return "Cannot divide by zero";
+    }
+
 
 // Addition
 function add(a, b) {
@@ -12,15 +16,11 @@ function add(a, b) {
 function subtract(a, b) {
     return a - b;
 }
-
-// Division
-function divide(a, b) {
-    if (b !== 0) {
-        return a / b;
-    } else {
-        return "Cannot divide by zero";
-    }
+}// Multiplication
+function multiply(a, b) {
+    return a * b;
 }
+
 
 // Examples
 let resultAddition = add(5, 3);
